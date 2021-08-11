65e28b66aa4ce5317074c91e6dcb34b4bca45759
Hitesh <kotechahitesh517@gmail.com>
2021-08-11 16:26:08 +0530

    moving dotenv file

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config({ path: './server/.env' })
+require('dotenv').config()
 
 const router = express.Router();
 
