56b41556e79cea35c854b652fa13bd1a04f510a6
Hitesh <kotechahitesh517@gmail.com>
2021-08-24 21:56:37 +0530

    Added docker files

---
+++ Makefile
@@ -0,0 +1,5 @@
+build:
+	docker build -t api-server .
+
+run-dev:
+	docker compose up
--- server/app.js
+++ server/app.js
@@ -16,13 +16,13 @@ const posts = require('./routes/api/posts');
 app.use("/api/posts", posts);
 
 // Handle Production
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
     // Static folder
     app.use(express.static(__dirname + '/public/'))
 
     // Handle SPA
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
-}
+// }
 
 app.listen(port, () => {
     console.log(`listing on port ${port}`)
